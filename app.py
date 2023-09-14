543fac9e24aef466a3f3c6316409c133bb78a1a3
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-14 14:34:13 +0100

    add post method to create a film record

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # TUTORIAL LINK https://levelup.gitconnected.com/full-stack-web-app-with-python-react-and-bootstrap-backend-8592baa6e4eb
 
 from flask import Flask, request, jsonify
-import requests
 from flask_cors import CORS
 import sqlite3 as sql # sql lite module
 # create the Flask app and configure the app to allows access to our endpoints from any ip-address using CORS
@@ -32,6 +31,21 @@ def api_get_films():
 		film_collection.append(film)
 	
 	return jsonify(film_collection)
+
+@app.route('api/add/<', methods='POST')
+def api_add_film():
+	if request.method == 'POST':
+		title = request.args.get('title')
+		year_released = request.args.get('year_released')
+		rating = request.args.get('rating')
+		duration = request.args.get('duration')
+		genre = request.args.get('genre')
+		
+		dbcon = sql.connect("filmflix.db")
+		dbCursor = dbcon.cursor()
+		dbCursor.execute("INSERT INTO tblfilms(title, yearReleased, rating, duration, genre) VALUES(?, ?, ?, ?, ?)", (title, year_released, rating, duration, genre))
+		dbcon.commit()
+		return jsonify({'test': 'test'})
 
 if __name__ == '__main__':
 	app.debug = True
